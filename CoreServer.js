dd3657a0767149becd6eef05630d0471736addbc
TaiShan  Lin <taishanlin@TaiShans-MacBook-Pro.local>
2022-01-07 09:35:36 -0500

    latest

--- CoreServer.js
+++ CoreServer.js
@@ -12,21 +12,6 @@ const fileUpload = require('express-fileupload');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Launch Express App
-const allowedOrigins = ['http://localhost:3000', 'http://localhost:3001'];
-app.use((req, res, next) => {
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-        res.setHeader('Access-Control-Allow-Origin', origin);
-    } else {
-        res.setHeader('Access-Control-Allow-Origin', allowedOrigins[0]);
-    }
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, auth-token, Authorization, stripe-signature, APPS');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    return next();
-});
-
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -38,25 +23,45 @@ if (process.env.INFRASTRUCTURE === "AWS") {
     const path = require('path');
     const fs   = require("fs")
     const filePath = path.join(__dirname, 'rds-combined-ca-bundle.pem');
-    mongoose.connect(process.env.AWS_URI,
-        {
-            ssl             :   true,
-            sslValidate     :   false,
-            sslCA: filePath
-        },
-        async (err) => {
-            if(err) {
-                console.log(err);
-            } else {
-                console.log('DocumentDB Connection Established');
-            }
+
+    var MongoClient = require('mongodb').MongoClient
+
+    // Create a MongoDB client, open a connection to DocDB; as a replica set,
+    // and specify the read preference as secondary preferred
+
+    var client = MongoClient.connect(
+     'mongodb://localhost:27017/utradea-test-docdb2',
+    {
+    tlsCAFile: `rds-combined-ca-bundle.pem`, //Specify the DocDB; cert
+    tlsAllowInvalidHostnames: true,
+    auth: {
+        username:'lintai',
+        password: 'Panda9686'
+    },
+    },
+    function(err, client) {
+        if(err)
+            throw err;
+
+        //Specify the database to be used
+        db = client.db('sample-database');
+
+        //Specify the collection to be used
+        col = db.collection('sample-collection');
+
+        //Insert a single document
+        col.insertOne({'hello':'Amazon DocumentDB'}, function(err, result){
+        //Find the document that was previously written
+        col.findOne({'hello':'DocDB;'}, function(err, result){
+            //Print the result to the screen
+            console.log(result);
+
+            //Close the connection
+            client.close()
         });
+    });
+});
 }
-
- app.get('/', (req, res) => {
-    res.send(`Welcome to tai's server running on port: ${port}`);
-  });
-
 // Listen to LocalHost:5000/
 var server = app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
